de6a11128d1b72ccccbc7d163367df54310e78a2
burrsutter <burrsutter@gmail.com>
2018-05-03 10:55:42 -0400

    simple echo

--- msgpusher.js
+++ msgpusher.js
@@ -3,15 +3,15 @@
 var container = require('rhea');
 
 container.on('connection_open', function (context) {
-    context.connection.open_receiver('reply_queue_99');
-    context.connection.open_sender('queue_99');
+    context.connection.open_receiver('queue_101');
+    context.connection.open_sender('queue_101');
 });
 container.on('message', function (context) {
     console.log("received back: " + context.message.body);
     context.connection.close();
 });
 container.on('sendable', function (context) {
-    context.sender.send("{text:'apple,orange,banana',replyTo:'reply_queue_99'}");
+    context.sender.send("{text:'apple,orange,banana',replyTo:'reply_queue_101'}");
     context.sender.detach();
 });
 container.connect({port: 5672, host: "127.0.0.1"});
