e1355d63e422e8c44ca58a37fed6052a713276c4
burrsutter <burrsutter@gmail.com>
2018-05-02 21:48:55 -0400

    tweaked to correct settings

--- msgpusher.js
+++ msgpusher.js
@@ -1,16 +1,17 @@
 
 
 var container = require('rhea');
+
 container.on('connection_open', function (context) {
-    context.connection.open_receiver('examples');
-    context.connection.open_sender('examples');
+    context.connection.open_receiver('reply_queue_99');
+    context.connection.open_sender('queue_99');
 });
 container.on('message', function (context) {
     console.log("received back: " + context.message.body);
     context.connection.close();
 });
 container.on('sendable', function (context) {
-    context.sender.send({body:'Hello World!'});
+    context.sender.send("{text:'apple,orange,banana',replyTo:'reply_queue_99'}");
     context.sender.detach();
 });
 container.connect({port: 5672, host: "127.0.0.1"});
