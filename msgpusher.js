d8761c3d31c880a9045e9bec652f57806d55c623
burrsutter <burrsutter@gmail.com>
2018-05-04 10:20:51 -0400

    body formatting

--- msgpusher.js
+++ msgpusher.js
@@ -3,15 +3,16 @@
 var container = require('rhea');
 
 container.on('connection_open', function (context) {
-    context.connection.open_receiver('queue_101');
-    context.connection.open_sender('queue_101');
+    // context.connection.open_receiver('queue_101');
+    context.connection.open_sender('queue_102');
 });
 container.on('message', function (context) {
     console.log("received back: " + context.message.body);
     context.connection.close();
 });
 container.on('sendable', function (context) {
-    context.sender.send("{text:'apple,orange,banana',replyTo:'reply_queue_101'}");
+    // context.sender.send("{\"text\":\"apple,orange,banana\",\"replyTo\":\"reply_queue_102\"}");
+    context.sender.send({body:{'somekey':'somevalue'}});
     context.sender.detach();
 });
 container.connect({port: 5672, host: "127.0.0.1"});
